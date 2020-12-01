026328e1ae79efc6fd153777fa8377aa3a9db973
Koen van Gilst <koen@koenvangilst.nl>
2020-12-01 12:25:28 +0100

    fixing compat issues jest + next

--- jest.config.js
+++ jest.config.js
@@ -12,4 +12,12 @@ module.exports = {
   testPathIgnorePatterns: ["/node_modules/"],
   collectCoverageFrom: ["src/**/*.{ts,tsx}"],
   moduleFileExtensions: ["ts", "tsx", "js", "jsx", "json", "node"],
+  globals: {
+    // we must specify a custom tsconfig for tests because we need the typescript transform
+    // to transform jsx into js rather than leaving it jsx such as the next build requires.  you
+    // can see this setting in tsconfig.jest.json -> "jsx": "react"
+    "ts-jest": {
+      tsconfig: "tsconfig.jest.json",
+    },
+  },
 };
