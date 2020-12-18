fb1d2406c31571e316e33a87cb82ff781b9a9b32
Koen van Gilst <koen@koenvangilst.nl>
2020-12-18 16:17:39 +0100

    adds nice staggered animation

--- src/components/LessonList.tsx
+++ src/components/LessonList.tsx
@@ -1,8 +1,37 @@
 import { Heading, Wrap, WrapItem } from "@chakra-ui/react";
+import { motion } from "framer-motion";
 import React, { FC } from "react";
 import { Lesson } from "../types";
 import LessonCard from "./LessonCard";
 import Underline from "./ui/Underline";
+
+const variantsWrapper = {
+  pageInitial: {
+    transition: { staggerChildren: 0.07, delayChildren: 0.2 },
+    opacity: 0,
+  },
+  pageAnimate: {
+    opacity: 1,
+    transition: { staggerChildren: 0.1, staggerDirection: 1 },
+  },
+};
+
+const variantsCards = {
+  pageAnimate: {
+    y: 0,
+    opacity: 1,
+    transition: {
+      x: { stiffness: 1000, velocity: -10 },
+    },
+  },
+  pageInitial: {
+    y: 300,
+    opacity: 0,
+    transition: {
+      x: { stiffness: 1000, velocity: -10 },
+    },
+  },
+};
 
 export interface LessonListProps {
   lessons: Lesson[] | null;
@@ -17,10 +46,20 @@ const LessonList: FC<LessonListProps> = ({ lessons, heading }) => {
       <Heading as="h2" size="lg" mb={10} textColor="gray.900">
         <Underline>{heading}</Underline>
       </Heading>
-      <Wrap spacing={8}>
+      <Wrap
+        spacing={8}
+        as={motion.div}
+        initial="pageInitial"
+        animate="pageAnimate"
+        variants={variantsWrapper}
+      >
         {lessons.map((lesson) => {
           return (
-            <WrapItem key={lesson.slug}>
+            <WrapItem
+              key={lesson.slug}
+              as={motion.div}
+              variants={variantsCards}
+            >
               <LessonCard
                 slug={lesson.slug}
                 title={lesson.title}
